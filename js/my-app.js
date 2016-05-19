2e5e2772b1ab5c097bff0e0846fe6538909f7c02
Collab APPS Team <Sami@MacBook-Pro.local>
2016-05-19 08:09:27 +0200

    Major Update
    General Fixes and Edit Idea Page

--- js/my-app.js
+++ js/my-app.js
@@ -1,1 +1,40 @@
 var myApp=new Framework7({animateNavBackIcon:true});var $$=Dom7;var mainView=myApp.addView('.view-main',{dynamicNavbar:true,domCache:true});
+
+$(document).ready(function(){
+
+jQuery.each(jQuery('textarea[data-autoresize]'), function() {
+    var offset = this.offsetHeight - this.clientHeight;
+ 
+    var resizeTextarea = function(el) {
+        jQuery(el).css('height', 'auto').css('height', el.scrollHeight + offset);
+    };
+    jQuery(this).on('keyup input', function() { resizeTextarea(this); }).removeAttr('data-autoresize');
+});
+    
+
+$("textarea.title-input").keyup(function(){
+  
+    if ( $(".title-input").val().length >= 80 ) {
+        $(  );
+    }
+        
+});
+    
+function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            
+            reader.onload = function (e) {
+                $('.main-image-click').attr('src', e.target.result);
+            }
+            
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    
+$(".main-image-input").change(function(){
+        readURL(this);
+});
+
+    
+});
